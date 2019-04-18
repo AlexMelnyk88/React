ebf2cbba64aabb1d92514b9bb9e6ac9f40b2ee00
AlexMelnyk88 <44344277+AlexMelnyk88@users.noreply.github.com>
2019-04-18 19:49:54 +0300

    Update index.js

--- modern_gallery_app_2/src/index.js
+++ modern_gallery_app_2/src/index.js
@@ -4,3 +4,10 @@ import MainApp from './Components/MainApp';
 
 ReactDOM.render(<MainApp />, document.getElementById('root'));
 
+                             //  --------------PLEASE PAY ATTENTION---------------
+      // in order to everything works correctly you should in the file 
+      //  SearchBar.js 
+      // add  your own Api key.If you have an account on 
+      //  https://pixabay.com
+      // you can create a new key and simply add it to the file.You will see the text where you sholud put Api key.
+     
